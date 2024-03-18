e66962b3867b5c426ed2b2b4d978066f359d8763
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-03-18 22:29:47 +0300

    18.03.2024

--- rentacar/src/main/java/com/turkcell/rentacar/dataAccess/abstracts/BrandRepository.java
+++ rentacar/src/main/java/com/turkcell/rentacar/dataAccess/abstracts/BrandRepository.java
@@ -3,5 +3,8 @@ package com.turkcell.rentacar.dataAccess.abstracts;
 import com.turkcell.rentacar.entities.concretes.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
+    Optional<Brand> findByNameIgnoreCase(String brandName);
 }
